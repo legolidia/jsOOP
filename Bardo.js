2104c6663c39b9c074205f5c4b817ef6ef979ca9
Ricardo <1168599@mackenzie.br>
2023-05-24 15:37:37 -0300

    finalizado

--- Bardo.js
+++ Bardo.js
@@ -1,21 +1,26 @@
 import Conjuradores from "./Conjuradores.js";
 
 export default class Bardo extends Conjuradores {
-
-  listaMusica = [];
+  #listaMusica = [];
 
   constructor(mana, poder, listaMusica){
     super(mana, poder);
-    this.listaMusica = listaMusica;
+    this.#listaMusica = listaMusica;
   }
+get listaMusica(){
+  return this.#listaMusica;
+}
 
+set listaMusica(listaMusica){
+  this.#listaMusica = listaMusica;
+}
 
 aprenderMusica(musica){
-  this.listaMusica.push(musica);
+  this.#listaMusica.push(musica);
   }
 
 cantarMusica(index){
-    return `Cantou musica ${this.listaMusica[index]}`
+    return `Cantou musica ${this.#listaMusica[index]}`
     }
 
 }
