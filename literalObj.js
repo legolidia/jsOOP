9e9cde77ea922658a148f270a53a0ac6c793863b
Ricardo <1168599@mackenzie.br>
2023-05-23 21:24:39 -0300

    curso POO

--- literalObj.js
+++ literalObj.js
@@ -1,13 +1,29 @@
 const user = {
   name: "Dju",
   email: "dju@dju.com",
-  date: "2001/03=5/03",
-  role: "admin",
-  active: true,
+  date: "2001/05/03",
+  role: "student",
   showInfos: function() {
-    console.log(this.name, this.email, this.date);
+    console.log(this.name, this.email);
   }
 }
 
-user.showInfos()
-//heranca de prototipo
+//this: "isso"; "isso" é o objeto que está executando o código, só funciona 
+//quando tem um contexto atrelado
+
+const admin = {
+  nome: "admin",
+  email: "adm@admin.com",
+  role: "admin",
+  createSomething(){
+    console.log("something")
+  }
+}
+
+//heranca de prototipo **
+
+Object.setPrototypeOf(admin, user)
+admin.createSomething()
+admin.showInfos()
+
+//__proto__ é o prototipo do objeto, é o objeto que está sendo herdado - construtor do objeto pai
